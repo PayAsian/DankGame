5935294742638ef1c9d573cac7b7addf3959a8b8
PayAsian <PayAsian@yahoo.com>
2017-06-02 11:40:14 -0600

    Finished The Game

--- DankGame/Controller/Game1.cs
+++ DankGame/Controller/Game1.cs
@@ -71,6 +71,12 @@ namespace DankGame.Controller
 		// The music played during gameplay
 		private Song gameplayMusic;
 
+		//Number that holds the player score
+		private int score;
+		// The font used to display UI elements
+		private SpriteFont font;
+
+
 
 
 
@@ -83,64 +89,9 @@ namespace DankGame.Controller
 
 		}
 
-		private void AddEnemy()
-		{
-			// Create the animation object
-			Animation enemyAnimation = new Animation();
-
-			// Initialize the animation with the correct animation information
-			enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 170, 170, 2, 125, Color.White, 1f, true);
-
-			// Randomly generate the position of the enemy
-			Vector2 position = new Vector2(GraphicsDevice.Viewport.Width + enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height - 100));
-
-			// Create an enemy
-			Enemy enemy = new Enemy();
-
-			// Initialize the enemy
-			enemy.Initialize(enemyAnimation, position);
-
-			// Add the enemy to the active enemies list
-			enemies.Add(enemy);
-		}
-
-		private void UpdateEnemies(GameTime gameTime)
-		{
-
-			// Spawn a new enemy enemy every 1.5 seconds
-			if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime)
-			{
-				previousSpawnTime = gameTime.TotalGameTime;
-
-				// Add an Enemy
-				AddEnemy();
-			}
-
-			// Update the Enemies
-			for (int i = enemies.Count - 1; i >= 0; i--)
-			{
-				enemies[i].Update(gameTime);
-
-				if (enemies[i].Active == false)
-				{
-					if (enemies[i].Health <= 0)
-					{
-						// Add an explosion
-						AddExplosion(enemies[i].Position);
-						enemies.RemoveAt(i);
-					}
-				}
-			}
-			// Play the explosion sound
-			explosionSound.Play();
-
-
-
-
-		}
-
 		private void UpdatePlayer(GameTime gameTime)
 		{
+
 
 			// Get Thumbstick Controls
 			player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
@@ -176,10 +127,43 @@ namespace DankGame.Controller
 
 				// Add the projectile, but add it to the front and center of the player
 				AddProjectile(player.Position + new Vector2(player.Width / 2, 0));
+				// Play the laser sound
+				laserSound.Play();
 			}
-			// Play the laser sound
-			laserSound.Play();
 
+			if (currentKeyboardState.IsKeyDown(Keys.Q))
+			{
+				// Reset our current time
+				previousFireTime = gameTime.TotalGameTime;
+
+				// Add the projectile, but add it to the front and center of the player
+
+				// Play the laser sound
+				laserSound.Play();
+			}
+
+			if (currentKeyboardState.IsKeyDown(Keys.A))
+			{
+				// Reset our current time
+				previousFireTime = gameTime.TotalGameTime;
+
+				// Add the projectile, but add it to the front and center of the player
+				// Play the laser sound
+				laserSound.Play();
+			}
+
+			// reset score if player health goes to zero
+			if (player.Health <= 0)
+			{
+				player.Health = 100;
+				score = 0;
+			}
+			// reset score if player health goes to zero
+			if (player.Health <= 0)
+			{
+				player.Health = 100;
+				score = 0;
+			}
 		}
 
 		/// <summary>
@@ -195,8 +179,15 @@ namespace DankGame.Controller
 			// Initialize the player class
 			player = new Player();
 
+			//Set player's score to zero
+			score = 0;
+
+
 			// Set a constant player move speed
-			playerMoveSpeed = 8.0f;
+			playerMoveSpeed = 6.0f;
+
+			bgLayer1 = new ParallaxingBackground();
+			bgLayer2 = new ParallaxingBackground();
 
 			// Initialize the enemies list
 			enemies = new List<Enemy>();
@@ -212,10 +203,15 @@ namespace DankGame.Controller
 
 			projectiles = new List<Projectile>();
 
-			explosions = new List<Animation>();
-
 			// Set the laser to fire every quarter second
 			fireTime = TimeSpan.FromSeconds(.15f);
+
+			explosions = new List<Animation>();
+
+			//Set player's score to zero
+			score = 0;
+
+
 
 			base.Initialize();
 		}
@@ -229,37 +225,40 @@ namespace DankGame.Controller
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			// Load the player resources 
+			// Load the player resources
+			Animation playerAnimation = new Animation();
+			Texture2D playerTexture = Content.Load<Texture2D>("Texture/Penguin");
+			playerAnimation.Initialize(playerTexture, Vector2.Zero, 128, 128, 2, 100, Color.White, 1f, true);
+
 			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
 
-			// Load the music
+			// Load the score font
+			font = Content.Load<SpriteFont>("Font/gameFont");
+
+			//Load the music
 			gameplayMusic = Content.Load<Song>("Sound/gameMusic");
 
-			// Load the laser and explosion sound effect
+			//Load the laser and explosion sound effect
 			laserSound = Content.Load<SoundEffect>("Sound/laserFire");
 			explosionSound = Content.Load<SoundEffect>("Sound/explosion");
 
 			// Start the music right away
 			PlayMusic(gameplayMusic);
 
-			player.Initialize(Content.Load<Texture2D>("Texture/Penguin"), playerPosition);
-
 			// Load the parallaxing background
-			bgLayer1.Initialize(Content, "Texture/bgLayer1", GraphicsDevice.Viewport.Width, -1);
-			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
+			bgLayer1.Initialize(Content, "Texture/myBackground2", GraphicsDevice.Viewport.Width, -1);
+			bgLayer2.Initialize(Content, "Texture/myBackground2", GraphicsDevice.Viewport.Width, -2);
 
-			enemyTexture = Content.Load<Texture2D>("Animation/Groot");
+			mainBackground = Content.Load<Texture2D>("Texture/KirbyBackground");
+
+			enemyTexture = Content.Load<Texture2D>("Animation/Bronco Burt");
 
 			projectileTexture = Content.Load<Texture2D>("Texture/laser");
 
 			explosionTexture = Content.Load<Texture2D>("Animation/explosion");
 
-
-			mainBackground = Content.Load<Texture2D>("Texture/CrossTheSpectrum");
-
-
-
-
+			// Load the score font
+			font = Content.Load<SpriteFont>("Font/gameFont");
 
 
 			//TODO: use this.Content to load your game content here 
@@ -297,12 +296,11 @@ namespace DankGame.Controller
 			// Update the enemies
 			UpdateEnemies(gameTime);
 
+			// Update the collision
+			UpdateCollision();
+
 			// Update the projectiles
 			UpdateProjectiles();
-
-			// Update the explosions
-			UpdateExplosions(gameTime);
-
 
 			// TODO: Add your update logic here
 
@@ -315,21 +313,21 @@ namespace DankGame.Controller
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw(GameTime gameTime)
 		{
-			// Start drawing
-			spriteBatch.Begin();
-
 			graphics.GraphicsDevice.Clear(Color.Teal);
 
 			//TODO: Add your drawing code here
 
-			// Update the collision
-			UpdateCollision();
+			// Start drawing
+			spriteBatch.Begin();
 
 			spriteBatch.Draw(mainBackground, Vector2.Zero, Color.White);
 
 			// Draw the moving background
 			bgLayer1.Draw(spriteBatch);
 			bgLayer2.Draw(spriteBatch);
+
+			// Draw the Player 
+			player.Draw(spriteBatch);
 
 			// Draw the Enemies
 			for (int i = 0; i < enemies.Count; i++)
@@ -349,12 +347,75 @@ namespace DankGame.Controller
 				explosions[i].Draw(spriteBatch);
 			}
 
-			// Draw the Player 
-			player.Draw(spriteBatch);
+			// Draw the score
+			spriteBatch.DrawString(font, "score: " + score, new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y), Color.Black);
+			// Draw the player health
+			spriteBatch.DrawString(font, "health: " + player.Health, new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + 30), Color.Black);
+
+			// Update the explosions
+			UpdateExplosions(gameTime);
+
 			// Stop drawing 
 			spriteBatch.End();
-
 			base.Draw(gameTime);
+		}
+
+		private void AddEnemy()
+		{
+			// Create the animation object
+			Animation enemyAnimation = new Animation();
+
+			// Initialize the animation with the correct animation information
+			enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 100, 100, 2, 100, Color.White, 1f, true);
+
+			// Randomly generate the position of the enemy
+			Vector2 position = new Vector2(GraphicsDevice.Viewport.Width + enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height - 100));
+
+			// Create an enemy
+			Enemy enemy = new Enemy();
+
+			// Initialize the enemy
+			enemy.Initialize(enemyAnimation, position);
+
+			// Add the enemy to the active enemies list
+			enemies.Add(enemy);
+		}
+
+		private void UpdateEnemies(GameTime gameTime)
+		{
+			// Spawn a new enemy enemy every 1.5 seconds
+			if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime)
+			{
+				previousSpawnTime = gameTime.TotalGameTime;
+
+				// Add an Enemy
+				AddEnemy();
+			}
+
+			// Update the Enemies
+			for (int i = enemies.Count - 1; i >= 0; i--)
+			{
+				enemies[i].Update(gameTime);
+
+				if (enemies[i].Active == false)
+				{
+					// If not active and health <= 0
+					if (enemies[i].Health <= 0)
+					{
+						// Add an explosion
+						AddExplosion(enemies[i].Position);
+						// Play the explosion sound
+						explosionSound.Play();
+						//Add to the player's score
+						score += enemies[i].ScoreValue;
+					}
+					enemies.RemoveAt(i);
+
+				}
+			}
+
+
+
 		}
 
 		private void UpdateCollision()
@@ -390,7 +451,6 @@ namespace DankGame.Controller
 					}
 				}
 			}
-
 			// Projectile vs Enemy Collision
 			for (int i = 0; i < projectiles.Count; i++)
 			{
@@ -410,6 +470,7 @@ namespace DankGame.Controller
 					}
 				}
 			}
+
 
 		}
 
@@ -433,6 +494,7 @@ namespace DankGame.Controller
 				}
 			}
 		}
+
 		private void AddExplosion(Vector2 position)
 		{
 			Animation explosion = new Animation();
@@ -468,5 +530,4 @@ namespace DankGame.Controller
 		}
 
 	}
-
 }
